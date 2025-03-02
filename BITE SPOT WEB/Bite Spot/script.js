16189d40f72fc33e1abe63f5332cae0680b2975e
Eman <emma.cielo.up@phinmaed.com>
2025-03-02 23:53:12 +0800

    clickable cards

--- BITE SPOT WEB/Bite Spot/script.js
+++ BITE SPOT WEB/Bite Spot/script.js
@@ -1,4 +1,3 @@
-// Redirect to homepage after 3 seconds
 setTimeout(function() {
     window.location.href = "homepage.html"; // Replace with the path to your homepage
 }, 3000); // 3000 milliseconds = 3 seconds
