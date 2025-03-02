30d5dd09ad07efaa5b4892098e70821cc787668e
Eman <emma.cielo.up@phinmaed.com>
2025-03-02 21:45:37 +0800

    small changes

--- BITE SPOT WEB/Bite Spot/script.js
+++ BITE SPOT WEB/Bite Spot/script.js
@@ -1,4 +1,4 @@
 // Redirect to homepage after 3 seconds
 setTimeout(function() {
-    window.location.href = "testHomepage.php"; // Replace with the path to your homepage
+    window.location.href = "homepage.html"; // Replace with the path to your homepage
 }, 3000); // 3000 milliseconds = 3 seconds
